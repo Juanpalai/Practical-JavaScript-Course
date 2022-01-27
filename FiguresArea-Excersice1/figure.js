ad2e363863072539295925e0a3839de1a798548c
Juan Pablo Lagos <juan.pablo.lagosi@gmail.com>
2022-01-27 12:59:39 +0900

    Area square

---
+++ FiguresArea-Excersice1/figure.js
@@ -0,0 +1,9 @@
+const ladocuadrado = 5;
+
+console.log("Los lados del cuadrado miden: " + ladocuadrado + "cm");
+
+var perimetroCuadrado = ladocuadrado * 4;
+console.log("El perimero del cuadrado es: " + perimetroCuadrado + "cm");
+
+var areaCuadrado = ladocuadrado * ladocuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm2");
